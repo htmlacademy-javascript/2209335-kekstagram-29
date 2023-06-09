60f911fe8c8676971792040e228565c78cfb9bf3
Keks <keks@htmlacademy.ru>
2023-06-09 10:04:31 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,24 @@
+function checkMaxLengthStr(str, maxLength) {
+  if (str.length <= maxLength) {
+    return true
+  } else {
+    return false
+  }
+}
+console.log("Hellow", checkMaxLengthStr("Hellow", 5));
+console.log("result", checkMaxLengthStr("result", 3));
+console.log("StartResult", checkMaxLengthStr("StartResult", 20));
+console.log("finish");
+
+
+const = result;
+function isPalindrome (str) {
+  const normalizeStr = str.replaceAll(" ").toUpperCase()
+  let resultStr = ''
+  for (let i = normalizeStr.length - 1; i >= 0; i--) {
+    resultStr += normalizeStr.at(i);
+  }
+  return resultStr === normalizeStr;
+}
+console.log("кекс", isPalindrome("кекс"));
+console.log("топот", isPalindrome("топот"));
