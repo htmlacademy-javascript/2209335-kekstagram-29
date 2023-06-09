c43ad6101f8b0116021fe597d7eb2ea22bcc8a97
Keks <keks@htmlacademy.ru>
2023-06-09 10:37:33 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -1,24 +1,23 @@
 function checkMaxLengthStr(str, maxLength) {
   if (str.length <= maxLength) {
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
 }
-console.log("Hellow", checkMaxLengthStr("Hellow", 5));
-console.log("result", checkMaxLengthStr("result", 3));
-console.log("StartResult", checkMaxLengthStr("StartResult", 20));
-console.log("finish");
+console.log('Hellow', checkMaxLengthStr('Hellow', 5));
+console.log('result', checkMaxLengthStr('result', 3));
+console.log('StartResult', checkMaxLengthStr('StartResult', 20));
+console.log('finish');
 
 
-const = result;
-function isPalindrome (str) {
-  const normalizeStr = str.replaceAll(" ").toUpperCase()
-  let resultStr = ''
+function isPalindrome(str) {
+  const normalizeStr = str.replaceAll(" ").toUpperCase();
+  let resultStr = '';
   for (let i = normalizeStr.length - 1; i >= 0; i--) {
     resultStr += normalizeStr.at(i);
   }
   return resultStr === normalizeStr;
 }
-console.log("кекс", isPalindrome("кекс"));
-console.log("топот", isPalindrome("топот"));
+console.log('кекс', isPalindrome('кекс'));
+console.log('топот', isPalindrome('топот'));
