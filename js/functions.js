5fe3e0b487f8c74f563eedcd1ff931413eb4e57d
Яна <1yana05@mail.ru>
2023-06-11 11:51:19 +0300

    добовляет дополнения по дз2

--- js/functions.js
+++ js/functions.js
@@ -11,13 +11,32 @@ console.log('StartResult', checkMaxLengthStr('StartResult', 20));
 console.log('finish');
 
 
-function isPalindrome(str) {
+const isPalindrome = (str) => {
   const normalizeStr = str.replaceAll(' ').toUpperCase();
-  let resultStr = '';
-  for (let i = normalizeStr.length - 1; i >= 0; i--) {
-    resultStr += normalizeStr.at(i);
-  }
-  return resultStr === normalizeStr;
-}
+  const reversedStr = normalizeStr.split().reverse().join();
+
+  return normalizeStr === reversedStr;
+};
+console.log('isPalindrome');
 console.log('кекс', isPalindrome('кекс'));
 console.log('топот', isPalindrome('топот'));
+console.log('Лёша на полке клопа нашёл', isPalindrome('Лёша на полке клопа нашёл'));
+
+
+const takeNum = (str) => {
+  let result = '';
+  const line = str.toString().split('');
+  const testAll = (item) => +item;
+  line.forEach((item) => {
+    if (!Number.isNaN(testAll(item))) {
+      result = result + item;
+    }
+  });
+  return +result;
+};
+console.log('takeNum');
+console.log(takeNum('2023 год'));
+console.log(takeNum('ECMAScript 2022'));
+console.log(takeNum('1 кефир, 0.5 батона'));
+console.log(takeNum('агент 007'));
+console.log(takeNum('а я томат'));
