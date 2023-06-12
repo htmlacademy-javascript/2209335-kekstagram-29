c27ac492ba0c7ca643fe32ea46550b5d5e81c703
Яна <1yana05@mail.ru>
2023-06-12 15:54:38 +0300

    исправляет третью функцию

--- js/functions.js
+++ js/functions.js
@@ -5,38 +5,41 @@ function checkMaxLengthStr(str, maxLength) {
     return false;
   }
 }
-console.log('Hellow', checkMaxLengthStr('Hellow', 5));
-console.log('result', checkMaxLengthStr('result', 3));
-console.log('StartResult', checkMaxLengthStr('StartResult', 20));
-console.log('finish');
+console.log('Проверяемая строка', checkMaxLengthStr('Проверяемая строка', 20));
+console.log('Проверяемая строка', checkMaxLengthStr('Проверяемая строка', 18));
+console.log('Проверяемая строка', checkMaxLengthStr('Проверяемая строка', 10));
 
 
 const isPalindrome = (str) => {
-  const normalizeStr = str.replaceAll(' ').toUpperCase();
-  const reversedStr = normalizeStr.split().reverse().join();
+  const normalizeStr = str.toLowerCase().replaceAll(' ','');
+  console.log(normalizeStr);
+  console.log(normalizeStr.split(''));
+  const reversedStr = normalizeStr.split('').reverse().join('');
 
   return normalizeStr === reversedStr;
 };
 console.log('isPalindrome');
-console.log('кекс', isPalindrome('кекс'));
-console.log('топот', isPalindrome('топот'));
-console.log('Лёша на полке клопа нашёл', isPalindrome('Лёша на полке клопа нашёл'));
+console.log(isPalindrome('топот'));
+console.log(isPalindrome('Довод'));
+console.log(isPalindrome('Кекс'));
+console.log(isPalindrome('Лёша на полке клопа нашёл'));
 
 
 const takeNum = (str) => {
   let result = '';
-  const line = str.toString().split('');
-  const testAll = (item) => +item;
-  line.forEach((item) => {
-    if (!Number.isNaN(testAll(item))) {
-      result = result + item;
+  for (const el of str.toString()) {
+    if (!Number.isNaN(parseInt(el, 10))){
+      result += el;
     }
-  });
-  return +result;
+  }
+  return +result !== 0 ? +result : NaN;
 };
+
 console.log('takeNum');
 console.log(takeNum('2023 год'));
 console.log(takeNum('ECMAScript 2022'));
 console.log(takeNum('1 кефир, 0.5 батона'));
 console.log(takeNum('агент 007'));
 console.log(takeNum('а я томат'));
+console.log(takeNum(-1));
+
