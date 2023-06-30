866b77ee5ef09273488d54f2c305c68f1cf9e97d
Яна <1yana05@mail.ru>
2023-06-30 11:19:31 +0300

    исправляет код

--- js/picture.js
+++ js/picture.js
@@ -2,13 +2,7 @@ import { constantQuantities } from './data.js';
 
 const picturesContainer = document.querySelector('.pictures');
 
-const listFragment = document.createDocumentFragment();
-
-constantQuantities.forEach((item) => {
-  listFragment.appendChild(generatePicture(item.url, item.description, item.comments, item.likes));
-});
-
-function generateImg(url, desc) {
+const generateImg = (url, desc) => {
   const img = document.createElement('img');
   img.classList.add('picture__img');
   img.setAttribute('width', '182');
@@ -16,43 +10,49 @@ function generateImg(url, desc) {
   img.setAttribute('alt', desc);
   img.setAttribute('src', url);
   return img;
-}
+};
 
-function generateInfo(commentsHtml, likesHtml) {
+const generateInfo = (commentsHtml, likesHtml) => {
   const pictureInfo = document.createElement('p');
   pictureInfo.classList.add('picture__info');
   pictureInfo.append(commentsHtml);
   pictureInfo.append(likesHtml);
   return pictureInfo;
-}
+};
 
-function generateLink() {
+const generateLink = () => {
   const link = document.createElement('a');
   link.setAttribute('href', '#');
   link.classList.add('picture');
   return link;
-}
+};
 
-function generatePictureComments(length) {
+const generatePictureComments = (length) => {
   const pictureComments = document.createElement('span');
   pictureComments.classList.add('picture__comments');
-  pictureComments.innerHTML = length;
+  pictureComments.textContent = length;
   return pictureComments;
-}
+};
 
-function generatePictureLikes(length) {
+const generatePictureLikes = (length) => {
   const pictureLikes = document.createElement('span');
   pictureLikes.classList.add('picture__likes');
-  pictureLikes.innerHTML = length;
+  pictureLikes.textContent = length;
 
   return pictureLikes;
-}
+};
 
-function generatePicture(url, desc, comments, likes) {
+const generatePicture = (url, desc, comments, likes) => {
   const link = generateLink();
   link.append(generateImg(url, desc));
   link.append(generateInfo(generatePictureComments(comments.length), generatePictureLikes(likes)));
   return link;
-}
+};
+
+const listFragment = document.createDocumentFragment();
+
+constantQuantities.forEach((item) => {
+  listFragment.appendChild(generatePicture(item.url, item.description, item.comments, item.likes));
+});
 
 picturesContainer.appendChild(listFragment);
