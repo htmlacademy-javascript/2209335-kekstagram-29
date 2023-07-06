966a97fcc0f25dd30f62dca78b204dc8f05bfccc
Keks <keks@htmlacademy.ru>
2023-07-06 12:42:52 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,7 @@
+import './util.js';
+import './data.js';
+import './picture.js';
+import './previewPicture.js';
+import {constantQuantities} from './data.js';
+
+console.log('data', constantQuantities);
